a3a2b6a1153e05e8cf5b39d8f3b56853168a8ade
Eduardo Alexandre <sunnygosdk@gmail.com>
2025-06-08 20:13:33 -0300

    fix: remove old code from main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"github.com/sunnygosdk/go-chi-fullcycle-api/config"
-	"github.com/sunnygosdk/go-chi-fullcycle-api/database"
-)
-
 func main() {
-	log.Println("FullCycle API Starting...")
-	config := config.LoadConfig()
-	db, err := database.ConnectDB(config)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
 }
